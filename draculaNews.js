fce8d26f6d01e08966a2fcb1440a47f07d391391
chaara-dev <github@chaara.dev>
2025-07-21 15:36:29 -0700

    Add OP tag and simplify JS

--- draculaNews.js
+++ draculaNews.js
@@ -1,59 +1,24 @@
-import { getAllComments } from "../libs/dom-utils";
-import { paths } from "../libs/paths";
+const urlPattern = /\.com\/item\?id=\d+/;
 
-async function init(metadata) {
-    if (!metadata.item.isItem) {
-        return false;
-    }
+function highlightMatchingUsers() {
+    const opUser = document.querySelector('a.hnuser:nth-child(2)');
 
-    const customWidth = metadata.options.commentsIndentWidth;
-    const itemAuthor = metadata.item.by;
+    if (opUser) {
+        const opUserName = opUser.innerHTML.trim();
 
-    const comments = getAllComments();
+        const commentUsers = document.querySelectorAll('a.hnuser');
 
-    for (const comment of comments) {
-        // Indent-border
-        comment.querySelector("td.ind").classList.add("__rhn__comment-indent");
-
-        // Custom indent width
-        const indentImage = comment.querySelector("td.ind img");
-        const indentLevel = indentImage.width / 40;
-        indentImage.width = indentLevel * customWidth;
-        indentImage.dataset.indentLevel = indentLevel;
-
-        const commentAuthor = comment.querySelector("a.hnuser");
-        if (!commentAuthor) {
-            continue;
-        }
-
-        // Highlight-op-username
-        if (itemAuthor && itemAuthor === commentAuthor.innerText) {
-            commentAuthor.innerText += " [op]";
-            commentAuthor.classList.add("__rhn__highlight-op");
-        }
-    }
-
-    browser.runtime.onMessage.addListener((request) => {
-        if (request.indentWidth) {
-            for (const comment of comments) {
-                const indentImage = comment.querySelector("td.ind img");
-                indentImage.width =
-                    request.indentWidth * indentImage.dataset.indentLevel;
+        commentUsers.forEach(user => {
+            if (user.innerHTML.trim() === opUserName) {
+                user.classList.add('highlightOP');
+                if (user !== opUser) {
+                    user.innerHTML += " [OP]";
+                }
             }
-        }
-    });
-
-    return true;
+        });
+    }
 }
 
-const details = {
-    id: "comments-ui-tweaks",
-    pages: {
-        include: paths.comments,
-        exclude: [],
-    },
-    loginRequired: false,
-    init,
-};
-
-export default details;
+if (urlPattern.test(window.location.href)) {
+    highlightMatchingUsers();
+}
